9afe9db3af81ab61cefc6e306395d452a9b6096e
Костя <kde8707@gmail.com>
2025-10-30 22:40:39 +0500

    практика 7

--- Практика_6/MainWindow.xaml.cs
+++ Практика_6/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +19,92 @@ namespace Практика_6
     /// </summary>
     public partial class MainWindow : Window
     {
+        private People CurrentUser = new People();
+
         public MainWindow()
         {
             InitializeComponent();
+            Application.Current.Resources["CurrentUser"] = CurrentUser;
+            DataContext = CurrentUser;
+        }
+
+        private void MessageUser(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var currentUser = (People)Application.Current.Resources["CurrentUser"];
+                string jsonString = JsonSerializer.Serialize(currentUser);
+                int i = 0;
+                while (true)
+                {
+                    if (File.Exists($"D_{i.ToString().PadLeft(5, '0')}.json"))
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                string fileName = $"D_{i.ToString().PadLeft(5, '0')}.json";
+                File.WriteAllText(fileName, jsonString);
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int i = Convert.ToInt32(_ID.Text.ToString());
+                    string fileName = $"D_{i.ToString().PadLeft(5, '0')}.json";
+                if (File.Exists(fileName))
+                {
+                    string jsonString = File.ReadAllText(fileName);
+                    People people =JsonSerializer.Deserialize<People>(jsonString)!;
+                    if (people != null)
+                    {
+
+                        if (people.Password == _Password.Text.ToString())
+                        {
+                            ID.Content = i.ToString().PadLeft(5, '0');
+                            FIO.Content = $"{people.Name} {people.Last_Name} {people.Middle_Name}";
+                            Spec.Content = people.Specialisation;
+                            Pass.Content = people.Password;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Неверный пароль");
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пусто(");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Такого id нет");
+                }
+            }
+            catch (IOException a)
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void DeleteUser(object sender, RoutedEventArgs e)
+        {
+            var currentUser = (People)Application.Current.Resources["CurrentUser"];
+            currentUser.Name = "";
+            currentUser.Last_Name = "";
+            currentUser.Middle_Name = "";
+            currentUser.Password="";
+            currentUser.Specialisation = "";
         }
     }
 }
---
+++ Практика_6/People.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_6
+{
+    public class People : INotifyPropertyChanged
+    {
+        private string name;
+        private string last_name;
+        private string middle_name;
+        private string specialisation;
+        private string password;
+
+        public string Name
+        {
+            get => name;
+            set { name = value; OnPropertyChanged(); }
+        }
+
+        public string Last_Name
+        {
+            get => last_name;
+            set { last_name = value; OnPropertyChanged(); }
+        }
+        public string Middle_Name
+        {
+            get => middle_name;
+            set { middle_name = value; OnPropertyChanged(); }
+        }
+
+        public string Specialisation
+        {
+            get => specialisation;
+            set { specialisation = value; OnPropertyChanged(); }
+        }
+        public string Password
+        {
+            get => password;
+            set { password = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
